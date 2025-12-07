d2a449c8c5e2eefa1428d1a7526ed750dd727375
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2025-12-07 17:33:56 -0600

    Update postCreate.sh

--- postCreate.sh
+++ postCreate.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # install poetry
-curl -sSL https://install.python-poetry.org | python3 -
+# curl -sSL https://install.python-poetry.org | python3 -
 # install containerlab
-curl -sL https://containerlab.dev/setup | sudo -E bash -s "all"
+bash -c "$(curl -sL https://get.containerlab.dev)"
 # install tools
-sudo apt install -y iputils-ping net-tools openssh-client python3-pip python3-venv containerd
+# sudo apt install -y iputils-ping net-tools openssh-client python3-pip python3-venv containerd
 # install gitlab-runner
-# curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
-# sudo apt-get install gitlab-runner
+curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
+sudo apt-get install -y gitlab-runner
 # Download GitLab Runner Docker image
-docker pull gitlab/gitlab-runner:latest
+# docker pull gitlab/gitlab-runner:latest
+docker pull registry.gitlab.com/jeffkala/workshop-implementing-cicd-pipelines:latest
+docker pull ghcr.io/astral-sh/uv:0.5-python3.11-bookworm-slim
